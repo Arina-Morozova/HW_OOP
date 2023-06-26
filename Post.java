5f67a3f81fea0af35eae7da0a04dc1828543cfd0
Neto test <aryamorozkova555@gmail.com>
2023-06-26 11:49:14 +0300

    Initial commit

---
+++ Post.java
@@ -0,0 +1,8 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronimyc;
+    public String phone;
+    public String surname;
+    public String subscription;
+}
